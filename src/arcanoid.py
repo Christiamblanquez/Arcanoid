7b16360d00c132a74ecb183f8cc47e93568b49d3
CHRISTIAM MARTÍNEZ BLÁNQUEZ <CHRISTIAM MARTNEZ BLNQUEZ>
2016-10-19 20:51:36 +0200

    Colisiones y poniendo a los malos

--- src/arcanoid.py
+++ src/arcanoid.py
@@ -9,10 +9,10 @@ class Bola(pygame.sprite.Sprite):
         self.image = load_image("images/ball.png", True)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
-        self.rect.centery = HEIGHT / 1.1
+        self.rect.centery = HEIGHT / 1.2
         self.speed = [0.5, -0.5]
         
-    def actualizar(self, time):
+    def actualizar(self, time, pala_jug):
         self.rect.centerx += self.speed[0] * time
         self.rect.centery += self.speed[1] * time
         if self.rect.left <= 0 or self.rect.right >= WIDTH:
@@ -22,6 +22,12 @@ class Bola(pygame.sprite.Sprite):
             self.speed[1] = -self.speed[1]
             self.rect.centery += self.speed[1] * time
             
+        if pygame.sprite.collide_rect(self, pala_jug):
+            self.speed[1] = -self.speed[1]
+            self.rect.centerx += -self.speed[0] * time
+            
+
+            
 def load_image(filename, transparent=False):  
     try:
         image = pygame.image.load(filename)    
@@ -30,24 +36,32 @@ def load_image(filename, transparent=False):
         raise SystemExit(message)    
     return image.convert_alpha()
 
-class Pala(pygame.sprite.Sprite):
-    def __init__(self, x):
+class ovnis(pygame.sprite.Sprite):
+    def __init__(self, x,y):
         pygame.sprite.Sprite.__init__(self)
         self.image = load_image("images/pala.png")
         self.image=pygame.transform.rotate(self.image, 90)
         self.rect = self.image.get_rect()
-        self.rect.centerx = x
-        self.rect.centerx = WIDTH / 2
-        self.rect.centery = HEIGHT / 1.1
+        self.rect.centerx = WIDTH / x
+        self.rect.centery = HEIGHT / y
+
+class Pala(pygame.sprite.Sprite):
+    def __init__(self, x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image("images/pala.png")
+        self.image=pygame.transform.rotate(self.image, 90)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / x
+        self.rect.centery = HEIGHT / y
         self.speed = 0.5
  
     def mover(self, time, keys):
-        if self.rect.top >= 0:
-            if keys[K_UP]:
-                self.rect.centery -= self.speed * time
-        if self.rect.bottom <= HEIGHT:
-            if keys[K_DOWN]:
-                self.rect.centery += self.speed * time
+        if self.rect.left >= 0:
+            if keys[K_LEFT]:
+                self.rect.centerx -= self.speed * time
+        if self.rect.right <= WIDTH:
+            if keys[K_RIGHT]:
+                self.rect.centerx += self.speed * time
  
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -55,7 +69,8 @@ def main():
  
     background_image = load_image('images/fondo.jpg')
     bola = Bola()
-    pala_jug = Pala(30)
+    pala_jug = Pala(2,1.1)
+    ovni = ovnis(2,3)
  
     clock = pygame.time.Clock()
  
@@ -66,10 +81,11 @@ def main():
             if eventos.type == QUIT:
                 sys.exit(0)
  
-        bola.actualizar(time)
+        bola.actualizar(time, pala_jug)
         pala_jug.mover(time, keys)
         screen.blit(background_image, (0, 0))
         screen.blit(bola.image, bola.rect)
+        screen.blit(ovni.image, ovni.rect)
         screen.blit(pala_jug.image, pala_jug.rect)
         pygame.display.flip()
     return 0

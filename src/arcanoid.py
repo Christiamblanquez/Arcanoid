e9ff94059fd805eaaea7de40ebc6f69897d20c7f
CHRISTIAM MARTÍNEZ BLÁNQUEZ <CHRISTIAM MARTNEZ BLNQUEZ>
2016-10-19 18:25:31 +0200

    Con fondo

--- src/arcanoid.py
+++ src/arcanoid.py
@@ -1,16 +1,30 @@
 import sys, pygame
 from pygame.locals import *
- 
+
 WIDTH = 640
 HEIGHT = 480
+
+def load_image(filename, transparent=False):  
+    try:
+        image = pygame.image.load(filename)    
+    except pygame.error as message:   
+        print("Cannot load image: " + filename)
+        raise SystemExit(message)    
+    return image.convert_alpha()
  
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Pruebas Pygame")
+ 
+    background_image = load_image('images/fondo.jpg')
+ 
     while True:
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
+ 
+        screen.blit(background_image, (0, 0))
+        pygame.display.flip()
     return 0
  
 if __name__ == '__main__':

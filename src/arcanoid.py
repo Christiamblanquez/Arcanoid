ed4a002da110025e0674ade0928eb6a9cf119136
CHRISTIAM MARTÍNEZ BLÁNQUEZ <CHRISTIAM MARTNEZ BLNQUEZ>
2016-10-19 18:58:00 +0200

    la pelota se mueve

--- src/arcanoid.py
+++ src/arcanoid.py
@@ -12,6 +12,16 @@ class Bola(pygame.sprite.Sprite):
         self.rect.centery = HEIGHT / 1.1
         self.speed = [0.5, -0.5]
         
+    def actualizar(self, time):
+        self.rect.centerx += self.speed[0] * time
+        self.rect.centery += self.speed[1] * time
+        if self.rect.left <= 0 or self.rect.right >= WIDTH:
+            self.speed[0] = -self.speed[0]
+            self.rect.centerx += self.speed[0] * time
+        if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
+            self.speed[1] = -self.speed[1]
+            self.rect.centery += self.speed[1] * time
+            
 def load_image(filename, transparent=False):  
     try:
         image = pygame.image.load(filename)    
@@ -27,11 +37,15 @@ def main():
     background_image = load_image('images/fondo.jpg')
     bola = Bola()
  
+    clock = pygame.time.Clock()
+ 
     while True:
+        time = clock.tick(60)
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
  
+        bola.actualizar(time)
         screen.blit(background_image, (0, 0))
         screen.blit(bola.image, bola.rect)
         pygame.display.flip()
